6aea9389d0524ad75b37c8d6c4c31b85d2b80d2c
DDEfromOR <daniel@ddevans.com>
2018-09-12 14:11:34 -0700

    Start adding in a QnA bot

---
+++ Bot Builder Echo Bot2/QuestionBot1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Prompts;
+
+/// <summary>
+/// A simple question bot built off of the online tutorial found at:
+/// https://docs.microsoft.com/en-us/azure/bot-service/bot-builder-tutorial-waterfall?view=azure-bot-service-4.0&tabs=cstab
+/// </summary>
+public class QuestionBot1 : IBot
+{
+    private readonly DialogSet dialogs;
+    public QuestionBot1()
+	{
+        dialogs = new DialogSet();
+	}
+}
